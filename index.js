257b57304c2c59f94611ae828b54371a57dab2b4
casnoe17 <casnoe17@gmail.com>
2022-02-14 21:06:48 -0600

    added prompts for dragon encounter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Your Code Here+
+//Your Code Here+
 let firstAnswer  = window.prompt('Do you head left or right?')
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
@@ -16,6 +16,23 @@ Do you follow it, or continue on your path?`)
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
-lead you away from the dragon altogether. Which path do you take?`)
-
+lead you away from the dragon altogether. Which do you choose?`)
+    if (secondAnswer === 'past the dragon'){
+        let secondAnswer = window.prompt('The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:')
+        if (secondAnswer === 'stay'){
+            let secondAnswer = window.prompt('You and the dragon have an uplifting conversation about local politics and become lifelong friends.')
+        }
+        else if (secondAnswer === 'away'){
+            let secondAnswer = window.prompt("Quickly, you run back to the cave's entrance. Sheepish, you return home.")
+        }
+    }
+    if (secondAnswer === 'away'){
+        let secondAnswer = window.prompt('After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?')
+            if(secondAnswer === 'draw it'){
+                let secondAnswer = window.prompt('You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.')
+            }
+            else if (secondAnswer === 'pick it'){
+                let secondAnswer = window.prompt('You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.')
+            }
+        }
 }
